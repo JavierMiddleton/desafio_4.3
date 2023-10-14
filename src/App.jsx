87698cda3f73718c419878e23d879cba17c60b01
Javier Middleton <javier.middleton13@gmail.com>
2023-10-14 11:58:46 -0300

    sacado comentario inutil

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,11 @@ function App() {
 
   const handleSearch = (searchTerm) => {
     const filtered = colaboradores.filter((colaborador) => {
-      // Comprueba si el término de búsqueda se encuentra en cualquier campo del colaborador
       const searchTermLower = searchTerm.toLowerCase();
       return Object.values(colaborador).some((field) =>
         field.toLowerCase().includes(searchTermLower)
       );
     });
-
     setFilteredColaboradores(filtered);
   };
 
